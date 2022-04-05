5ce47c28bc003c247383865a1380095ce68f5052
StasSaltykov <stasyanych92@gmail.com>
2022-04-06 01:26:54 +0300

    hw#34

---
+++ src/components/Timer.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+
+type Props = {
+    interval?: number;
+    zone: string;
+    timezone: string;
+}
+const Timer: React.FC<Props> = ({interval, zone, timezone}) => {
+    const [time, setTime] = React.useState(new Date());
+    function tic(): void {
+        setTime(new Date());
+    }
+    useEffect(() => {
+        setInterval(tic, interval || 1000);
+    }, [])
+    
+    return <div style={{marginLeft: '50vw'}}>
+        <h3>{zone}</h3>
+        <label>{time.toLocaleTimeString('en-GB', {timeZone: timezone})}</label>
+    </div>
+    ;
+}
+export default Timer;
