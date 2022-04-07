28fc0aa3cd165ddfe87aa96daa07b28cb0f8d3be
StasSaltykov <stasyanych92@gmail.com>
2022-04-07 18:33:11 +0300

    hw fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.css';
 import Timer from './components/Timer';
 
 function App() {
-  return <><Timer zone={'New-York Time'} timezone={'America/New_York'}/>
+  return <div><Timer zone={'New-York Time'} timezone={'America/New_York'}/>
            <Timer zone={'Jerusalem Time'} timezone={'Asia/Jerusalem'}/>
            <Timer zone={'London Time'} timezone={'Europe/London'}/>
            <Timer zone={'Berlin Time'} timezone={'Europe/Berlin'}/>
-        </>
+        </div>
 }
 
 export default App;
